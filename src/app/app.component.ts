d30f6c1050bbf20a44079329e7a143d4b89fbedc
Felipe Hora <l.felipe.hora@gmail.com>
2018-10-12 01:23:53 -0300

    commit de aula concluída - 12.10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+class Cliente{
+  nome: string;
+  email: string;
+  profissao: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  cliente = new Cliente();
+
+  profissoes = ['Desenvolvedor de sistemas', 'Analista de sistemas', 'Administrador de BD'];
+  profissaoPadrao = 'Uma coisa legal';
+
+  salvar(form: NgForm){
+    // this.cliente.nome = form.value.nome;
+    // this.cliente.email = form.value.email;
+    // this.cliente.profissao = form.value.profissao;
+    console.log(form);
+    // console.log(this.cliente);
+  }
 }
